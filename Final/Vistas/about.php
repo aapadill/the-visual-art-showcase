4ccfbbfd2dcc59dcf371e656d5dfe6127f6d914e
Aaron Padilla <x@deblish.com>
2024-01-25 02:46:26 +0200

    about us
    static page

---
+++ Final/Vistas/about.php
@@ -0,0 +1,29 @@
+<?php
+require_once('../config/cargador.php');
+use Controladores\Router;
+
+include Router::direccion('/plantillas/header.php');
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+            <h1>About us</h1>
+            <p class="lead">Supporting Emerging Artists</p>
+            <p>
+                Our project emerged from the need to support emerging artists who lack the resources to exhibit their works in physical galleries and need a platform to showcase their art. We aim to address these needs by creating an online platform that not only displays the artists' work but also provides detailed information about each piece, the artist, and their creative process.
+            </p>
+            <p>
+                We've identified that many emerging artists struggle to promote their works online, as existing options are often limited to massive platforms focused more on sales than on the art itself. Our website is designed to enhance the user experience in interacting with art and offer an accessible, friendly platform for emerging artists to exhibit and potentially sell their works online in the future.
+            </p>
+            <h2>Project Objectives</h2>
+            <p>
+                The primary goal of our project is to improve user interaction with art through gestures and animations, increase each artist's presence through temporary exhibitions on our page, and provide easy access to contact information for emerging artists. Furthermore, we plan to integrate an online store and subscription in the future, enabling users to purchase their favorite artworks directly from the artists. No comissions, no middle men.
+            </p>
+        </div>
+    </div>
+</div>
+
+<?php
+include Router::direccion('/plantillas/footer.php');
+?>
